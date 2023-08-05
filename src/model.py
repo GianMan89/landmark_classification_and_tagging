6830dd32d90793cf5fbb682f0e8b903af0ed28d8
Gianluca Manca <gianluca.manca@de.abb.com>
2023-08-05 20:14:51 +0200

    Update: added CNN model class

--- src/model.py
+++ src/model.py
@@ -5,20 +5,67 @@ import torch.nn as nn
 # define the CNN architecture
 class MyModel(nn.Module):
     def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
-
         super().__init__()
 
-        # YOUR CODE HERE
-        # Define a CNN architecture. Remember to use the variable num_classes
+        # YOUR CODE HERE: Define a CNN architecture. Remember to use the variable num_classes
         # to size appropriately the output of your classifier, and if you use
         # the Dropout layer, use the variable "dropout" to indicate how much
         # to use (like nn.Dropout(p=dropout))
+
+        self.model = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3, out_channels=16, kernel_size=3, padding=1
+            ),  # -> [batch_size, 16, 224, 224]
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2, stride=2
+            ),  # -> [batch_size, 16, 112, 112]
+            nn.Conv2d(
+                in_channels=16, out_channels=32, kernel_size=3, padding=1
+            ),  # -> [batch_size, 32, 112, 112]
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2, stride=2
+            ),  # -> [batch_size, 32, 56, 56]
+            nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=3, padding=1
+            ),  # -> [batch_size, 64, 56, 56]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2, stride=2
+            ),  # -> [batch_size, 64, 28, 28]
+            nn.Conv2d(
+                in_channels=64, out_channels=128, kernel_size=3, padding=1
+            ),  # -> [batch_size, 128, 28, 28]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2, stride=2
+            ),  # -> [batch_size, 128, 14, 14]
+            nn.Conv2d(
+                in_channels=128, out_channels=256, kernel_size=3, padding=1
+            ),  # -> [batch_size, 256, 14, 14]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2, stride=2
+            ),  # -> [batch_size, 256, 7, 7]
+            nn.Flatten(),  # -> [batch_size, 256 * 7 * 7]
+            nn.Linear(256 * 7 * 7, 1024),  # -> [batch_size, 1024]
+            nn.Dropout(p=dropout),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, num_classes),
+        )  # -> [batch_size, num_classes]
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # YOUR CODE HERE: process the input tensor through the
         # feature extractor, the pooling and the final linear
         # layers (if appropriate for the architecture chosen)
-        return x
+        return self.model(x)
 
 
 ######################################################################################
@@ -35,7 +82,6 @@ def data_loaders():
 
 
 def test_model_construction(data_loaders):
-
     model = MyModel(num_classes=23, dropout=0.3)
 
     dataiter = iter(data_loaders["train"])
